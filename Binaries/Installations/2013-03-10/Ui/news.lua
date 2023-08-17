9f7fc5fd4130b149ef19b9d86888b42fe72dc644
kuroppoi <68156848+kuroppoi@users.noreply.github.com>
2023-08-17 16:08:03 +0200

    Add client 2013-03-10

---
+++ Binaries/Installations/2013-03-10/Ui/news.lua
@@ -0,0 +1,250 @@
+--[[ All Location Ver.]]
+--2012/11/28 NL News.lua Update Ver.
+
+News_NLD = {
+
+-- PVE AttentionScreen
+       Notice01 = {
+       img = "notice-50.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Different Picture
+       Notice02 = {
+       img = "notice-49.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Enchant
+       Notice03 = {
+       img = "notice-42.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Emoticon
+       Notice04 = {
+       img = "notice-41.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Resolution
+       Notice05 = {
+       img = "notice-38.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Dancing
+       Notice06 = {
+       img = "notice-39.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Control
+       Notice07 = {
+       img = "notice-26.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Manual
+       Notice08 = {
+       img = "notice-00.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+}
+
+News_GER = {
+
+-- Different Picture
+	   Notice01 = {
+       img = "notice-14.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Enchant
+       Notice02 = {
+       img = "notice-09.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Emoticon
+       Notice03 = {
+       img = "notice-07.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- ItemCrane
+       Notice04 = {
+       img = "notice-05.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Resolution
+       Notice05 = {
+       img = "notice-04.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Dancing
+       Notice06 = {
+       img = "notice-03.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Control
+       Notice07 = {
+       img = "notice-02.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Manual
+       Notice08 = {
+       img = "notice-00.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+}
+
+News_USA = {
+-- Different Picture
+	   Notice01 = {
+       img = "notice-11.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Abount Enchant System
+       Notice02 = {
+       img = "notice-09.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Emoticon Use Guide
+       Notice03 = {
+       img = "notice-07.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Item Crane Guide
+       Notice04 = {
+       img = "notice-05.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Resolution Upgrade
+       Notice05 = {
+       img = "notice-04.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Shall We Dance
+       Notice06 = {
+       img = "notice-03.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Ctrl Key Use Guide
+       Notice07 = {
+       img = "notice-02.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+
+-- Basic Control Key Guide
+       Notice08 = {
+       img = "notice-00.png",
+                
+        buttons = {
+            --btn0 = { cmd = "open_cardshop",},
+            --btn1 = { cmd = "open_guide",},        
+        },
+    },
+}
